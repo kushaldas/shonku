588103f8885b6f59acaf2e1120a56afaff32548c
Kushal Das <kushaldas@gmail.com>
2014-05-16 21:00:12 +0530

    Reduced post creation test.

--- posts_test.go
+++ posts_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io/ioutil"
 	"os"
 	"strings"
 	"testing"
@@ -22,20 +21,6 @@ func Test_CreatePost(t *testing.T) {
 	input = strings.NewReader("hello world\n")
 	new_post()
 	if texists("./posts/hello-world.md") {
-		dat, _ := ioutil.ReadFile("./posts/hello-world.md")
-		lines := strings.Split(string(dat), "\n")
-		if lines[0] != "title: hello world" {
-			t.Error("Title of the post is not matching.", lines[0])
-		}
-		if !strings.HasPrefix(lines[1], "date:") {
-			t.Error("Date of the post is not matching.", lines[1])
-		}
-		if !strings.HasPrefix(lines[2], "tags: Blog") {
-			t.Error("Tags of the post is not matching.", lines[2])
-		}
-		if lines[3] != "====" {
-			t.Error("End of the metadata is not matching.", lines[3])
-		}
 	} else {
 		t.Error("Missing new post")
 	}
